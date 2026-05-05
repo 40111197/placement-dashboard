94336e76aa44dc4c43ba0f324050cf9a100c368a
SHIVARAMAKRISHNAN T <shivaramakrishnan@SHIVARAMAKRISHNANs-Laptop.local>
2026-05-05 09:31:34 +0530

    chore: remove config.js from gitignore to fix db fetching in production

---
+++ config.js
@@ -0,0 +1,4 @@
+window.ENV = {
+    SUPABASE_URL: 'https://rnuuhgiqymukzojiflry.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJudXVoZ2lxeW11a3pvamlmbHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NDEzMDUsImV4cCI6MjA5MDUxNzMwNX0.IeKHzPrc4Hlz5_jjHUfy_crdavE38sI-IRkwuO9ES84'
+};
